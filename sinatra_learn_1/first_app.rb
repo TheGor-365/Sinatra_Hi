6c52cf8af0da847769fe0f2be01bcfd07ae1005c
Gor <7495500@gmail.com>
2020-04-12 02:41:39 +0300

    Sinatra first app

---
+++ sinatra_learn_1/first_app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @login = params[:login]
+  @password = params[:password]
+
+  if @login == 'admin' && @password == 'secret'
+    
+    erb :welcome
+  else
+    erb :index
+  end
+end
